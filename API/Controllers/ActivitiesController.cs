c18dbcdc08ccb3849dc22199356edeb5d1f88861
nikhilpothuru <pothurunikhil@gmail.com>
2020-02-05 10:14:46 -0800

    Added Activites Put, Delete

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -31,9 +31,23 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        public async Task<ActionResult<Unit>> Create([FromBody] Create.Command command)
         {
             return await _mediator.Send(command); 
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, [FromBody] Edit.Command command)
+        {
+            command.Id = id; 
+            return await _mediator.Send(command); 
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        { 
+            return await _mediator.Send(new Delete.Command(id)); 
         }
     }
 }
